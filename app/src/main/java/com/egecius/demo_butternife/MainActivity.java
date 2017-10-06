cb8e8359406569016a834898c1a355c523e1f22b
Egis <e.gecius@gmail.com>
2017-10-06 07:54:40 +0100

    add @OnClick

--- app/src/main/java/com/egecius/demo_butternife/MainActivity.java
+++ app/src/main/java/com/egecius/demo_butternife/MainActivity.java
@@ -1,13 +1,24 @@
 package com.egecius.demo_butternife;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.show_toast)
+    void showToast() {
+        Toast.makeText(this, "show toast clicked", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
